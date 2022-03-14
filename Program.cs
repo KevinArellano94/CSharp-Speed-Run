41d0b11e607acf6a590a71e49ca2cc22db201405
Kevin Arellano <Kevin.Arellano94@gmail.com>
2022-03-14 15:42:11 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csharpspeedrun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("First Name: ");
+            var first_name = Console.ReadLine();
+            var currentDateTime = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello {first_name}");
+            Console.WriteLine($"{Environment.NewLine}registered: {currentDateTime:U}");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+            
+        }
+    }
+}
